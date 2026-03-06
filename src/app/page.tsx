4eae799a7cc24211001abe0e21fc8f5b227993c4
contato461 <contato@pauloferrari.com>
2026-03-06 10:21:46 -0300

    update scene

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import * as BABYLON from "@babylonjs/core";
@@ -10,9 +9,6 @@ import { Scene } from "@babylonjs/core/scene";
 import { Engine } from "@babylonjs/core/Engines/engine";
 import { Vector3 } from "@babylonjs/core/Maths/math.vector";
 import { SceneLoaderFlags } from "@babylonjs/core/Loading/sceneLoaderFlags";
-import { HavokPlugin } from "@babylonjs/core/Physics/v2/Plugins/havokPlugin";
-
-import HavokPhysics from "@babylonjs/havok";
 
 import "@babylonjs/core/Loading/loadingScreen";
 import "@babylonjs/core/Loading/Plugins/babylonFileLoader";
@@ -23,7 +19,6 @@ import "@babylonjs/core/Cameras/Inputs/freeCameraVirtualJoystickInput";
 import "@babylonjs/core/Meshes/groundMesh";
 import "@babylonjs/core/Lights/directionalLight";
 
-import "@babylonjs/core/Physics";
 import "@babylonjs/materials/sky";
 
 import { loadScene } from "babylonjs-editor-tools";
@@ -60,17 +55,6 @@ export default function Home() {
 
   async function handleLoad(engine: Engine, scene: Scene) {
 
-    // =========================
-    // HAVOK PHYSICS
-    // =========================
-
-    const havok = await HavokPhysics();
-
-    scene.enablePhysics(
-      new Vector3(0, -9.81, 0),
-      new HavokPlugin(true, havok)
-    );
-
     SceneLoaderFlags.ForceFullSceneLoadingForIncremental = true;
 
     await loadScene("/scene/", "example.babylon", scene, scriptsMap);
@@ -87,20 +71,16 @@ export default function Home() {
 
       camera.attachControl(canvas, true);
 
-      // posição inicial
-      camera.position = new Vector3(0, 2, -5);
+      // altura do observador
+      camera.position = new Vector3(0, 1.7, -5);
 
       camera.speed = 0.15;
       camera.angularSensibility = 4000;
-
-      // gravidade
-      scene.gravity = new Vector3(0, -0.5, 0);
 
       // colisões
       scene.collisionsEnabled = true;
 
       camera.checkCollisions = true;
-      camera.applyGravity = true;
 
       // corpo do player (~1.8m)
       camera.ellipsoid = new Vector3(0.4, 0.9, 0.4);
